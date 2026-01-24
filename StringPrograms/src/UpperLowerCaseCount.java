e0cffb54091ad006499319dfe83f0f64b0bf5abe
Venkatesh-5764 <venkateshk5764@gmail.com>
2026-01-24 15:11:29 +0530

    added some codes like ToUpperLower case and to finding the spaces count and other things

--- StringPrograms/src/UpperLowerCaseCount.java
+++ StringPrograms/src/UpperLowerCaseCount.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class UpperLowerCaseCount {
     public static void main(String[] args){
         String sen;
-        int uc=0,lc=0;
+        int uc=0,lc=0,dc=0,spc=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the sentence user");
         sen=sc.nextLine();
@@ -15,6 +15,21 @@ public class UpperLowerCaseCount {
         System.out.println("The uppercase letters count is "+uc);
 
         System.out.println("The lowercase letters count is "+lc);
+        for (char ch:sen.replace(" ","").toCharArray()){
+            if(Character.isDigit(ch)){
+                dc++;
+            }
+        }
+        for(char ch:sen.toCharArray()){
+            if(ch==' '){
+                spc++;
+            }
+        }
+        System.out.println("The digits count is "+dc);
+        System.out.println("The space count is "+spc);
+        System.out.println("the Lowercase of the sentence is :"+sen.toLowerCase());
+
+        System.out.println("the Lowercase of the sentence is :"+sen.toUpperCase());
     }
 
 }
