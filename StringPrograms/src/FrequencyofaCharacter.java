abafaef2063008c9e7e819fb33b227105ace1c63
Venkatesh-5764 <venkateshk5764@gmail.com>
2026-01-26 22:33:49 +0530

    added code for the first repeating and non repeating character in the given string

--- StringPrograms/src/FrequencyofaCharacter.java
+++ StringPrograms/src/FrequencyofaCharacter.java
@@ -24,13 +24,19 @@ public class FrequencyofaCharacter {
             charrfreq.put(chh,charrfreq.getOrDefault(chh,0)+1);
         }
         System.out.println(charrfreq);
+       // today we will try to find the first non repeating character and also the first repeating character in the given string
+        //now we will see the first non repeating character and here i am using the previous code variables like
+        // the string variable name is sen and the hashmap variable is charfreq; lets do it
+
         for(char ch:sen.toCharArray()){
-            if(charfreq.get(ch)==1){
-                System.out.println("the first non repeating character is : "+ch);
+            if(charfreq.get(ch)==1){// just mmodify this we get the non repeating character;
+                System.out.println("the first non repeating character of the given string is : "+ch);
                 return;
             }
         }
 
+        // so we can see here the first non repeating character is d and first repeating character is i so in this way we can find out the outuput and thank for today
+        // see you in another string code and if you find any mistake in this code or if you can optimize this code you can mention in the comments thank you ..........................................:)
     }
 
 
